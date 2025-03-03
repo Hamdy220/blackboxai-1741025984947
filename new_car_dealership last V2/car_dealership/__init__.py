5f8051e344e08d8d2f8b52ab3135ace87c2c2439
Hamdy220 <Hamdynassar65@gmail.com>
2025-03-03 20:20:37 +0200

    Add new_car_dealership last V2/car_dealership/__init__.py 1741026034665

---
+++ new_car_dealership last V2/car_dealership/__init__.py
@@ -0,0 +1,18 @@
+"""
+نظام إدارة معرض أبو ريا للسيارات
+"""
+
+__version__ = '1.0.0'
+__author__ = 'BLACKBOXAI'
+
+# لا نقوم باستيراد الوحدات هنا لتجنب الاستيراد الدائري
+__all__ = [
+    'main',
+    'database',
+    'login',
+    'car_management',
+    'client_management',
+    'transaction_management',
+    'utils',
+    'backup_manager'
+]
