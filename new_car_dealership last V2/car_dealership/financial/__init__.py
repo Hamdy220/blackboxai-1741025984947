8dd861a26cc20e092e8adbe02c111b9deddb97b4
Hamdy220 <Hamdynassar65@gmail.com>
2025-03-03 20:21:01 +0200

    Add new_car_dealership last V2/car_dealership/financial/__init__.py 1741026059417

---
+++ new_car_dealership last V2/car_dealership/financial/__init__.py
@@ -0,0 +1,5 @@
+from .finance_page import FinancePage
+from .accounting import AccountingManager
+from .installments import InstallmentsManager
+from .invoices import InvoicesManager
+from .reports import ReportsManager
